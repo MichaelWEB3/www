77b4f41f436894442f3f909d31bb5e6abe6e4925
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2021-09-20 11:06:54 -0300

    build :)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.73b651ae077ceee9fb9fd1eedb317f54.js"
+  "./precache-manifest.1be523c785135e5bfbe9120ed9a7b28a.js"
 );
 
 self.addEventListener('message', (event) => {
