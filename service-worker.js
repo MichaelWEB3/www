3f353fd5e00f345743873ccdf0476905d7815699
mik3 <m3ribeiroo@gmail.com>
2022-01-21 17:31:10 -0300

    .

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0f851e413518e281fb69b8851e7f86bd.js"
+  "./precache-manifest.cc767b994193e4856dead047d929e394.js"
 );
 
 self.addEventListener('message', (event) => {
