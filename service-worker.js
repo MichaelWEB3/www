d90907d092b8fa4fd5022a75939355e538f3946e
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2022-02-04 18:10:06 -0300

    minha conta e relatorio

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7ba5fae68841dd2a004fcf4d62384134.js"
+  "./precache-manifest.0bef37d99542c353160dcb5729eafa46.js"
 );
 
 self.addEventListener('message', (event) => {
